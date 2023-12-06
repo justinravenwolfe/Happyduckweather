b096d2904f3b7b41bc38a56c9120d766186cd0d6
justinravenwolfe <147559260+justinravenwolfe@users.noreply.github.com>
2023-12-05 19:24:40 -0500

    created search button and buttons appear

--- index.js
+++ index.js
@@ -4,30 +4,37 @@ const key="54618fe1a12009457244e8835544de58";
 // Your OpenWeather API Key
 
 // The city you want the forecast for
-var city_in = document.getElementById("search");
-var city = city_in.value; 
+//Container for all the buttons
 
 // The URL to the 5 day / 3 hour forecast API
 
 
-// Function to get weather data
 function getWeatherForecast() {
-//Creates the specific link
-const url = 'https://api.openweathermap.org/data/2.5/forecast?q=' +city+'&units=metric&appid='+key;
-//Fetch call to grab the data
-fetch(url)
-.then(response => {
-  if (response.ok) {
-     return response.json();
-  } else {
-     throw new Error('Network response was not ok.');
+  var city = document.getElementById("city").value;
+  const url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + encodeURIComponent(city) + '&units=metric&appid=' + key;
+  
+  fetch(url)
+      .then(response => {
+        if (response.ok) {
+         return response.json();
+        } else {
+         throw new Error('Network response was not ok: ' + response.statusText);
+        }
+      })
+      .then(data => {
+        console.log(data); // Log the data object
+        //Stores the city's name
+        var city_name = data.city.name; 
+        var new_btn = document.createElement('button'); 
+        //The button will have the name of the city they searched for
+        new_btn.textContent = city_name; 
+        new_btn.classList.add("btn");
+        new_btn.classList.add("btn-primary"); 
+        new_btn.classList.add("col-2"); 
+        document.getElementById("city_opts").appendChild(new_btn);
+      })
+      .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+      });
   }
-  })
-  .then(data => {
-    console.log(data); // Log the data object
-  })
-  .catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
-}
 
