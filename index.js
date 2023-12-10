29965d3cd543db4a66769ec1be30b5494e5b3d4b
justinravenwolfe <147559260+justinravenwolfe@users.noreply.github.com>
2023-12-10 12:44:06 -0500

    made basic weather cards

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //Should not be editable/protects it
 const key="54618fe1a12009457244e8835544de58";
+var images = ["https://www.freeiconspng.com/thumbs/sun-icon/sun-icon-4.jpg","https://www.freepnglogos.com/uploads/cloud-clipart/clipart-clouds-picture-images-0.png","https://banner2.cleanpng.com/20180322/brq/kisspng-rain-cloud-storm-weather-clip-art-rain-5ab47b2dee2374.9241528315217774539754.jpg"]; 
+var type  = ["sun", "cloud", "rain"]; 
 
 // Your OpenWeather API Key
 
@@ -32,9 +34,68 @@ function getWeatherForecast() {
         new_btn.classList.add("btn-primary"); 
         new_btn.classList.add("col-2"); 
         document.getElementById("city_opts").appendChild(new_btn);
+        var weather_cards= document.getElementById('weather_cards');
+        var weather_list = data.list; 
+        //Start at position 1 <- i = 0
+        //Go all the way to the end of the list -> .length
+        //Move in steps of 8 
+        //3 hours * 8 = 24 hours
+        for(var i = 0; i< weather_list.length; i+=8){
+        var curr_day = weather_list[i];
+        //.dt_txt <- holds the date
+        //MM/DD/YYYY
+        var date = Date(curr_day.dt_txt);
+        /*
+        A hierarchal structure of key value pairs
+        Weather_Json {
+          'main'{
+            'tmp'{
+              90 degrees
+            }
+          }
+        }
+        */
+        var temp = curr_day.main.temp;
+        var weather = curr_day.weather[0].main; 
+        //Outer counter for the card
+        var card_container = document.createElement('div');
+        card_container.classList.add('weather_card');
+        //Restrict the width
+        card_container.classList.add('col-2'); 
+        //H2 for the date
+        var calendar = document.createElement('h4');
+        calendar.id = "curr_date";
+        calendar.textContent = date; 
+        calendar.style.textDecoration = 'underline'; 
+        //H3 for the Temperature
+        var degrees= document.createElement('h5');
+        degrees.id = "curr_temp";
+        degrees.textContent = temp + "°C"; 
+        //paragraph for the weather description 
+        var desc = document.createElement('h4'); 
+        desc.id = "weather_desc";
+        desc.textContent = weather; 
+        var weather_img = document.createElement('img');
+        var desc_opt = weather;
+        if(desc_opt== "Clear"){
+          weather_img.url = images[0];
+        } else if(desc_opt == "Cloudy"){
+          weather_img.url = images[1];
+        }else{
+          weather_img.url = images[2];
+        }
+        //Append these element onto the main div
+        card_container.appendChild(calendar);
+        card_container.appendChild(degrees);
+        card_container.appendChild(desc); 
+        card_container.append(weather_img); 
+        //Actually puts it on the webpage
+        weather_cards.appendChild(card_container); 
+      }
       })
       .catch(error => {
       console.error('There has been a problem with your fetch operation:', error);
       });
   }
-
+//Defining a new function<- can call it whatever you want
+//Every 8 elements is a new day
